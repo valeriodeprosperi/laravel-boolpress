026adc13b20a830eecc7ccd7b5e309fe93718b9a
valerio <valerio.deprosperi@gmail.com>
2020-11-10 16:22:06 +0100

    route

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix("admin")->namespace("Admin")->middleware("auth")->group(function () {
+    Route::get("/", "HomeController@index")->name("home");
+    
+});
